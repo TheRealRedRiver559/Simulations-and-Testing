505dbe540f8052fc00021f02c2c9bc0400521ec0
Redriver <80642468+TheRealRedRiver559@users.noreply.github.com>
2022-03-06 16:56:07 -0800

    Create settings.py

---
+++ Sand Simulation/settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    WIDTH = 800
+    HEIGHT = 800
+    SIZE = (WIDTH, HEIGHT)
+
+    MAX_FPS = 100
+
+    CELL_SIZE = 5 #cell size in pixels
+
+    window = None
+    columns = HEIGHT // CELL_SIZE
+    rows = WIDTH // CELL_SIZE
